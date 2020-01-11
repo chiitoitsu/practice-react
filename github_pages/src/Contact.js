6836b7f61b3f1eae2f543291eaf3fb0a835d234b
chiitoitsu <wanwan@tong.dongyang.ac.kr>
2020-01-11 21:09:45 +0900

    Update Contact

--- github_pages/src/Contact.js
+++ github_pages/src/Contact.js
@@ -3,12 +3,13 @@ import './Contact.css';
 
 function Contact() {
     return (
-        <div id='Contact'>
+        <form id='Contact' method='post' action='https://formspree.io/email@email.com'>
             <div id='Contact_Title'>Get in Touch</div>
-            <input id='Contact_Email' placeholder='Your Email'/>
-            <input id='Contact_Message' placeholder='Your Message'/>
-            <button id='Contact_Button'>Send</button>
-        </div>
+            <input type="hidden" name="_subject" value="Contact request from personal website" />
+            <input id='Contact_Email' name="_replyto" placeholder='Your Email' type='email' required />
+            <textarea id='Contact_Message' name="message" placeholder='Your Message' cols='80' rows='15' required />
+            <input id='Contact_Button' type='submit' value='Send' />
+        </form>
     )
 }
 
